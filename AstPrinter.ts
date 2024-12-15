0373e755d9a9f293b280387f79c1042fc78b4691
kino6052 <113693383+nino-ux@users.noreply.github.com>
2024-12-15 19:59:16 +0300

    Chapter 5: Representing Code

---
+++ AstPrinter.ts
@@ -0,0 +1,28 @@
+import { Binary, Expr, Grouping, Literal, Unary, Visitor } from "./Expr.ts";
+
+export class AstPrinter implements Visitor<string> {
+  print(expr: Expr): string {
+    return expr.accept(this);
+  }
+
+  visitBinaryExpr(expr: Binary): string {
+    return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  visitGroupingExpr(expr: Grouping): string {
+    return this.parenthesize("group", expr.expression);
+  }
+
+  visitLiteralExpr(expr: Literal): string {
+    return expr.value?.toString() ?? "nil";
+  }
+
+  visitUnaryExpr(expr: Unary): string {
+    return this.parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  private parenthesize(name: string, ...exprs: Expr[]): string {
+    const parts = exprs.map((expr) => expr.accept(this));
+    return `(${name} ${parts.join(" ")})`;
+  }
+}
